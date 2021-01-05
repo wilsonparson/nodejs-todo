f08bdf3b126cbd146c1c977b3b9458f064dd5d8e
Wilson Parson <wilson.parson@gmail.com>
2021-01-04 21:56:08 -0700

    Beginnings of POST request

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,20 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import { Todo } from './types';
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.use(bodyParser.json());
+
+app.get('/todos', (req, res) => {
+  res.send('Todos');
+});
+
+app.post('/todos', (req, res) => {
+  const todo = req.body;
+  console.log(todo);
+  res.send(`Todo created: ${JSON.stringify(todo)}`);
 });
 
 app.listen(port, () => {
